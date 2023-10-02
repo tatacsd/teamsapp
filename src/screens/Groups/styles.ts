c3e3f165254a3e9846b5416f41e1db36e794ce79
thayscasado <thayscasado@hotmail.com>
2023-10-01 21:22:41 -0700

    CSS-in-JS - implement theme colors

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,9 +2,9 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  background-color: #000;
-  align-items: center;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
   justify-content: center;
+  align-items: center;
 `;
 
 export const Title = styled.Text`
