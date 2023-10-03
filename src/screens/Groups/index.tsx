dad5e6b4d017b9c505dcf5b0aea181efdf726ed1
thayscasado <thayscasado@hotmail.com>
2023-10-03 14:42:40 -0700

    CSS-in-JS - add flatlist with some group examples

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,13 +1,28 @@
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 import { Container } from './styles';
+
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([
+    'Group 1',
+    'Group 2',
+    'Group 3',
+  ]);
   return (
     <Container>
       <Header />
       <Highlight title="Groups" subtitle="Play with your friends" />
-      <GroupCard title="Group 1" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        showsVerticalScrollIndicator={false}
+        style={{ marginTop: 24 }}
+      />
     </Container>
   );
 }
