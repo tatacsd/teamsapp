e8d7c79a94d42855a74e56217123dd3c3e872877
thayscasado <thayscasado@hotmail.com>
2023-10-06 23:59:22 -0700

    Navigation - implemented safe area view

--- src/screens/Players/styles.ts
+++ src/screens/Players/styles.ts
@@ -1,6 +1,7 @@
+import { SafeAreaView } from 'react-native-safe-area-context';
 import styles, { DefaultTheme, css } from 'styled-components/native';
 
-export const Container = styles.View`
+export const Container = styles(SafeAreaView)`
     flex: 1;
     padding: 24px;
     background-color: ${({ theme }: { theme: DefaultTheme }) =>
