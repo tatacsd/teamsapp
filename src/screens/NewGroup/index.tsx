ce26a549f6429cd430ec36bad5df2bd216be58a7
thayscasado <thayscasado@hotmail.com>
2023-10-07 01:10:10 -0700

    ♻️ add customized error message and app error class to app

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -4,7 +4,9 @@ import { Highlight } from '@components/Highlight';
 import { Input } from '@components/Input';
 import { useNavigation } from '@react-navigation/native';
 import { groupCreate } from '@storage/group/groupCreate';
+import { AppError } from '@utils/AppError';
 import { useState } from 'react';
+import { Alert } from 'react-native';
 import { Container, Content, Icon } from './styles';
 
 export function NewGroup() {
@@ -13,10 +15,38 @@ export function NewGroup() {
 
   const handleNewGroup = async () => {
     try {
+      if (group.trim().length === 0) {
+        Alert.alert('New Group', 'Group name is required.');
+        return;
+      }
+
+      const regex = /^[a-zA-Z0-9]+$/;
+      if (!regex.test(group)) {
+        Alert.alert(
+          'New Group',
+          'Group name can only contain letters and numbers.'
+        );
+        return;
+      }
+
+      if (group.length < 3 || group.length > 20) {
+        Alert.alert(
+          'New Group',
+          'Group name must be between 3 and 20 characters.'
+        );
+        return;
+      }
+
       await groupCreate(group);
       navigate('players', { group });
     } catch (error) {
-      console.log(error);
+      if (error instanceof AppError) {
+        Alert.alert('New Group', error.message);
+        return;
+      } else {
+        Alert.alert('New Group', 'Something went wrong, try again later.');
+        console.log(error);
+      }
     }
   };
 
--- src/storage/group/groupCreate.ts
+++ src/storage/group/groupCreate.ts
@@ -1,10 +1,17 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { GROUP_COLLECTION } from '@storage/storageConfig';
+import { AppError } from '@utils/AppError';
 import { groupGetAll } from './groupsGetAll';
 
 export async function groupCreate(newGroupName: string) {
   try {
     const storedGroups = await groupGetAll();
+
+    // Check if group already exists
+    if (storedGroups.includes(newGroupName)) {
+      throw new AppError('Group already exists');
+    }
+
     await AsyncStorage.setItem(
       GROUP_COLLECTION,
       JSON.stringify([...storedGroups, newGroupName])
