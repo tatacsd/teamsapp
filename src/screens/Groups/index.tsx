097519e8a79965f48a38a619f1539914b847f41c
thayscasado <thayscasado@hotmail.com>
2023-10-06 23:45:12 -0700

    5384724: Navigation - following documentation to navigate through screens v2

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,28 +4,15 @@ import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { ListEmpty } from '@components/ListEmpty';
 import { useNavigation } from '@react-navigation/native';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
 import { Container } from './styles';
 
-export type RootParamList = {
-  groups: undefined;
-  players: {
-    group: string;
-  };
-  newGroup: undefined;
-};
-
-type GroupsProps = {
-  navigation: NativeStackNavigationProp<RootParamList, 'groups'>;
-};
-
-export function Groups({ navigation }: GroupsProps) {
+export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
   const { navigate } = useNavigation();
   const handleNewGroup = () => {
-    navigation.navigate('newGroup');
+    navigate('newGroup');
   };
   return (
     <Container>
